394974253ee289e2af84d623a069cb74a04d2f0c
Maina, Hernán J <hernan.maina@mi.unc.edu.ar>
2025-04-11 03:52:07 -0300

    First commit

---
+++ js/custom.js
@@ -0,0 +1,71 @@
+const moon_icon = "fa-regular fa-moon";
+const sun_icon = "fa-regular fa-sun";
+const dark_font = "section-font-dark";
+const light_font = "section-font-light";
+const dark_button = "btn btn-sm btn-outline-secondary";
+const light_button = "btn btn-sm btn-outline-dark";
+const dark_links = "social-links-dark";
+const light_links = "social-links-light";
+
+const light_avatar = "/images/avatar_bw.png"
+const dark_avatar = "/images/avatar_color.png"
+
+
+const darkTheme = () => {
+
+    document.querySelector('body').setAttribute('data-bs-theme', 'dark');
+    document.querySelector('body').setAttribute('class', dark_font);
+    document.querySelector('#btn-follow').setAttribute('class', dark_button);
+    
+    document.querySelectorAll('#avatar_img').forEach(element => {
+        element.setAttribute('src', dark_avatar);
+    });
+
+    document.querySelectorAll('#theme-mode-icon').forEach(element => {
+        element.setAttribute('class', sun_icon);
+    });
+
+    document.querySelectorAll('#social-links').forEach(element => {
+        var c = element.getAttribute('class');
+        element.setAttribute('class', c.replace(light_links, dark_links));
+    });
+}
+
+const lightTheme = () => {
+    document.querySelector('body').setAttribute('data-bs-theme', 'light');
+    document.querySelector('body').setAttribute('class', light_font);
+    document.querySelector('#btn-follow').setAttribute('class', light_button);
+    
+    document.querySelectorAll('#avatar_img').forEach(element => {
+        element.setAttribute('src', light_avatar);
+    });
+
+    document.querySelectorAll('#theme-mode-icon').forEach(element => {
+        element.setAttribute('class', moon_icon);
+    });
+
+    document.querySelectorAll('#social-links').forEach(element => {
+        var c = element.getAttribute('class');
+        element.setAttribute('class', c.replace(dark_links, light_links));
+    });
+}
+
+const applyManualTheme = () => {
+    var mode = document.querySelector('body').getAttribute('data-bs-theme');
+    if (mode == 'light') {
+        darkTheme();
+    } else {
+        lightTheme();
+    };
+};
+
+const applyThemeBasedOnTime = () => {
+    const currentHour = new Date().getHours();
+    if (currentHour >= 20 || currentHour < 7) {
+        darkTheme();
+    } else {
+        lightTheme();
+    }
+};
+
+applyThemeBasedOnTime();
